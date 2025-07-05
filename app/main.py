6c92c894626bd9750751fe1f65166a8e8a336264
Renatoinvest <renatovenancionovaes26@gmail.com>
2025-07-04 21:58:47 -0300

    Delete app/main.py

--- app/main.py
+++
@@ -1,8 +0,0 @@
-# Código principal do app Streamlit
-
-import streamlit as st
-import pandas as pd
-
-df = pd.read_csv('data/df_final.csv')
-st.title('📊 Análise de Desempenho Escolar')
-st.write(df.head())
